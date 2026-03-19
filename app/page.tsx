a4462699f4f39bfad77f9697d5134267cee5be99
7717CMI <coherentdrive@gmail.com>
2026-03-19 16:22:42 +0530

    commit

--- app/page.tsx
+++ app/page.tsx
@@ -204,7 +204,7 @@ export default function DashboardPage() {
                 Coherent Dashboard
               </h1>
               <h2 className="text-sm text-black">
-                {dashboardName || ' Africa Lifting and Offshore Equipment Solutions Market'}
+                {dashboardName || ' Angola Lifting and Offshore Equipment Solutions Market'}
               </h2>
             </div>
           </div>
